fd8c9ea0e9915a63d8e24e4ef37c2a48b359d350
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-17 14:00:35 +0100

    feat: cleared

--- src/contract/wrappers/UNftCollection.ts
+++
@@ -1,1 +0,0 @@
-export * from '../build/UNftCollection/tact_UNftCollection';
--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const NETWORK = "testnet";
-
-export const MAINNET_ADDRESSES = {
-  username: "0QDh9eI_Uqnu_ZcvsTZD1LKWsU4nhx-HRQAqvPVcJiNqs-ln",
-};
-
-export const TESTNET_ADDRESSES = {
-  username: "0QDh9eI_Uqnu_ZcvsTZD1LKWsU4nhx-HRQAqvPVcJiNqs-ln",
-};
