6910716e76260d0a2f9fea448b0af991e546facf
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-28 22:38:53 +0100

    feat: added telemetree

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { TwaAnalyticsProvider } from '@tonsolutions/telemetree-react';
 import AppProvider from './providers/AppProvider.tsx';
 import ModalProvider from './providers/ModalProvider.tsx';
+import App from './App.tsx';
+import env from './shared/env.ts';
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -17,16 +19,22 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <TonConnectUIProvider manifestUrl="https://tma.quyx.xyz/manifest/tonconnect.json">
-                <ModalProvider>
-                    <AppProvider>
-                        <BrowserRouter>
-                            <App />
-                        </BrowserRouter>
-                    </AppProvider>
-                </ModalProvider>
-            </TonConnectUIProvider>
-        </QueryClientProvider>
+        <TwaAnalyticsProvider
+            projectId={env.TELEMETREE_PROJECT_ID}
+            apiKey={env.TELEMETREE_API_KEY}
+            appName={env.TELEMETREE_APP_NAME}
+        >
+            <QueryClientProvider client={queryClient}>
+                <TonConnectUIProvider manifestUrl="https://tma.quyx.xyz/manifest/tonconnect.json">
+                    <ModalProvider>
+                        <AppProvider>
+                            <BrowserRouter>
+                                <App />
+                            </BrowserRouter>
+                        </AppProvider>
+                    </ModalProvider>
+                </TonConnectUIProvider>
+            </QueryClientProvider>
+        </TwaAnalyticsProvider>
     </React.StrictMode>
 );
--- src/shared/env.ts
+++ src/shared/env.ts
@@ -1,6 +1,13 @@
 import { createAsyncLocalStorage } from './async.storage';
 
-const { BASE_URL, PROD, VITE_API_ENDPOINT } = import.meta.env;
+const {
+    BASE_URL,
+    PROD,
+    VITE_API_ENDPOINT,
+    VITE_TELEMETREE_PROJECT_ID,
+    VITE_TELEMETREE_API_KEY,
+    VITE_TELEMETREE_APP_NAME,
+} = import.meta.env;
 
 const API_ENDPOINT = VITE_API_ENDPOINT || 'http://localhost:3000/';
 const storage = createAsyncLocalStorage('tma');
@@ -10,4 +17,7 @@ export default {
     IS_PROD: PROD,
     API_ENDPOINT,
     storage,
+    TELEMETREE_PROJECT_ID: VITE_TELEMETREE_PROJECT_ID,
+    TELEMETREE_API_KEY: VITE_TELEMETREE_API_KEY,
+    TELEMETREE_APP_NAME: VITE_TELEMETREE_APP_NAME,
 };
